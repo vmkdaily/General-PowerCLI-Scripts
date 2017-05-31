6c528c9bde1438365a298faa365ff19964953a6b
Mike Nisk <webmaster@vmkdaily.com>
2017-05-31 17:11:46 -0600

    updated loader to prevent PowerCLI welcome message.

--- Invoke-VMNetworkReportXL.ps1
+++ Invoke-VMNetworkReportXL.ps1
@@ -270,70 +270,69 @@ Process {
 
         <#
         .DESCRIPTION
-          Imports PowerCLI Modules and/or Loads the PowerCLI Snapins as appropriate.
-          This function understands both snapins and modules so it supports loading
-          of PowerCLI versions 5.0 to 6.5+
+          Loader supporting PowerCLI 5.0 to 6.5.1 and later.
+          
         .NOTES
           Script:         Invoke-PowerLoader.ps1
           Based on:       http://wahlnetwork.com/2015/04/13/powercli-modules-snapins/
           Type:           Function
           Author:         Mike Nisk
           Organization:   vmkdaily
+    
+    
+        .EXAMPLE
+        . c:\mods\Invoke-PowerLoader.ps1
+        Invoke-PowerLoader
+        First we dot source the function.  Then we run with no parameters.
+        This quietly loads PowerCLI with no welcome message.
 
         .EXAMPLE
-        Invoke-PowerLoader
-        ABOUT MODULE AUTOLOADING
-          Once you upgrade to PowerCLI 6.5.1, the VMware.PowerCLI module will be autoloaded
-          upon first use for the given runtime session.  As such, there is no need for this
-          function after upgrading to 6.5.1 and later.  It doesn't hurt anything to load
-          it manually with this function, it is simply not needed on PowerCLI 6.5.1 and later.
+        Invoke-PowerLoader -Quiet:$false
+        This example returns the `Welcome to PowerCLI` message on PowerCLI 6.5.1
 
-       APPENDIX - How to upgrade to PowerCLI 6.5.1
-          This is not required for the function herein.  In fact, I show you this upgrade technique
-          so that you do not need a PowerCLI loader in the future.
-          #prepare
-          Upgrade Powershell to 5.1  #https://msdn.microsoft.com/en-us/powershell/wmf/5.1/install-configure
-          Uninstall PowerCLI from Control Panel > Add/Remove Programs  #or Start > Run > appwiz.cpl
-          Remove-Item 'C:\Program Files (x86)\VMware\Infrastructure\PowerCLI' #do not delete parent folders
-          Find-Module -Name VMware.PowerCLI #this gets your nuget setup if needed (i.e. to allow access to the PowerShell Gallery)
-
-          #install
-          Install-Module -Name VMware.PowerCLI -Scope AllUsers -AllowClobber -Force     #this is All Users
-          -or-
-          Install-Module -Name VMware.PowerCLI -Scope CurrentUser -AllowClobber -Force  #this is Current User Only
-          Note: The allow clobber is needed for PowerCLI to win
-          any conflicts against HyperV cmdlets that use the same names.
         #>
 
         [CmdletBinding()]
-        Param() #none
-
-        Begin {
-            $vMods = Get-Module -Name VMware* -ListAvailable -Verbose:$false
-        }
+        Param([bool]$Quiet = $true)
 
         Process {
 
+          #handle PowerCLI 6.5.1+ if using quiet mode
+          If((Get-Module -Name VMware.PowerCLI -ListAvailable) -and ($Quiet -eq $true)) {
+            $SkipLoader = $true
+            $vMods = $null
+          } #End If
+          Else {
+            #normal module loader, PowerCLI 6.3 to 6.5+ (includes welcome message)
+            $vMods = Get-Module -Name VMware* -ListAvailable -Verbose:$false
+          } #End Else
+
+          #If we don't seem to have PSGallery version, move on
+          If(-Not($SkipLoader)) {
+
+            #load PowerCLI 6.3 to 6.5
             If($vMods) {
                 foreach ($mod in $vMods) {
                 Import-Module -Name $mod -ErrorAction Stop -Verbose:$false
-              }
+              } #End Foreach
               Write-Verbose -Message 'PowerCLI 6.x Module(s) imported'
-            }
+            } #End If
             Else {
+              #fall-back to PSSnapin if needed
               If(!(Get-PSSnapin -Name VMware.VimAutomation.Core -ErrorAction SilentlyContinue)) {
                 Try {
                   Add-PSSnapin -Name VMware.VimAutomation.Core -ErrorAction Stop
-                  Write-Msg -InputMessage 'PowerCLI 5.x Snapin added; recommend upgrading to PowerCLI 6.x'
-                }
+                  Write-Verbose -Message 'PowerCLI 5.x Snapin added; recommend upgrading to PowerCLI 6.x'
+                } #End Try
                 Catch {
                   Write-Warning -Message ('{0}' -f $_.Exception.Message)
                   Write-Warning -Message 'Could not load PowerCLI!'
                   Throw 'PowerCLI 5.0 or later required'
-                }
-              }
-            }
-        }
+                } #End Catch
+              } #End If snapins
+            } #End Else vMods
+          } #End if SkipLoader
+        } #End Process
         End {
           Remove-Variable -Name vMods -Confirm:$false
         } #End End
